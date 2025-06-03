9f3cf66d9f944953f56a15b2d0bb83d267c96e34
Clara Mauro <maurog.clara@gmail.com>
2025-06-03 19:41:33 +0200

    initialisation projet

---
+++ styles/font.ts
@@ -0,0 +1,4 @@
+import { Playfair_Display, Source_Sans_3 } from "next/font/google";
+
+export const playfair = Playfair_Display({ variable: "--font-playfair", subsets: ["latin"] });
+export const sourcesans = Source_Sans_3({ variable: "--font-source-sans", subsets: ["latin"] });
